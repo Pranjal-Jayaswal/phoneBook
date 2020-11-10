b0e3d3654a15bb7158ab9f96b4778ee9ed04fdf8
Pranjal <pranjaljsl2910@gmail.com>
2020-11-10 13:17:44 +0530

    now using MongoDB as DB

--- phoneBook/index.js
+++ phoneBook/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const port = 8000;
 
 const db=require('./config/mongoose');
+const Contact=require ('./models/contact');
 
 const app = express();
 
@@ -28,28 +29,58 @@ let cl = [
 ];
 
 app.get('/', function (req, res) {
+   Contact.find({},function(err,contacts){
+    if(err){
+        console.log('errrorrr in fetching');
+        return;
+    }
     return res.render('index.ejs', {
         title: 'Contact List',
-        c_l: cl,
+        c_l: contacts,
     });
+   })
+   
+    // return res.render('index.ejs', {
+    //     title: 'Contact List',
+    //     c_l: cl,
+    // });
 });
 
 app.get('/delete_contact/',function(req,res){
-    let phone=req.query.phone;
-    //console.log(req.query);
-   let indexCount =cl.findIndex(num => num.phone==phone);
-    if(indexCount != -1){
-        cl.splice(indexCount, 1);
-    }
+   
+    let id=req.query.id;
+    Contact.findByIdAndDelete(id,function(err){
+        if(err){
+            console.log('error in deleting');
+            return;
+        }
+        return res.redirect('back');
 
-    return res.redirect('back');
+    })
+//     let phone=req.query.phone;
+//     //console.log(req.query);
+//    let indexCount =cl.findIndex(num => num.phone==phone);
+//     if(indexCount != -1){
+//         cl.splice(indexCount, 1);
+//     }
+
+//     return res.redirect('back');
 });
 
 app.post('/createContact',function(req,res){
-    cl.push(req.body);
-    res.redirect('/');
+  //  cl.push(req.body);
+    Contact.create({
+        name:req.body.name,
+        phone:req.body.phone,
+    },function(err,newContact){
+        if(err){
+            console.log('errrorrr');
+        }
+    })
+    console.log('new contact dbdb created')
+    return res.redirect('/');
 });
-
+  
 
 app.listen(port, function (err) {
     if (err) {
